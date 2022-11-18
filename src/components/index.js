6419dd25f2b8e158d09272f7424ce5248e8b5e05
alfnbntng <alfanbintang28@gmail.com>
2022-11-18 11:22:00 +0700

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Hasil from "./Hasil";
+import ListCategories from "./ListCategories";
+import NavbarComponent from "./NavbarComponent";
+import Menus from "./Menus";
+import TotalBayar from "./Totalbayar";
+
+export { Hasil, ListCategories, NavbarComponent, Menus, TotalBayar };
